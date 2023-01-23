a04b3e1d30b24f19ee2a4e7adffd073f4a8eb6f3
Salwa99 <s.ritage99@gmail.com>
2023-01-23 15:00:28 +0100

    Fix linters errors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,26 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createReducer } from '@reduxjs/toolkit';
 
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 
 const addBook = (book) => ({
-type: ADD,
-book,
+  type: ADD,
+  book,
 });
 
 const removeBook = (book) => ({
-type: REMOVE,
-book,
+  type: REMOVE,
+  book,
 });
 
-
 const bookReducer = createReducer([], {
-    [ADD]: (state, action) => {
+  [ADD]: (state, action) => {
     state.push(action.book);
-    },
-    
-    [REMOVE]: (state, action) => {
+  },
+
+  [REMOVE]: (state, action) => {
     state.filter((book) => book.id !== action.book.id);
-    },
+  },
 });
 
 export { addBook, removeBook };
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,16 +1,16 @@
 const CHECKED_STATUS = 'bookstore/categories/CHECKED_STATUS';
 
 const checkStatus = () => ({
-    type: CHECKED_STATUS,
+  type: CHECKED_STATUS,
 });
 
-const categoryReducer = (state = [], action = {} ) => {
-    switch (action.type) {
-        case CHECKED_STATUS:
-            return 'under construction';
-        default:
-            return state;
-    }
+const categoryReducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case CHECKED_STATUS:
+      return 'under construction';
+    default:
+      return state;
+  }
 };
 
 export { checkStatus };
